6f85c0bc05f6c8596bde7d7e4b80a3fefd5b8b30
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-21 23:59:15 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,24 @@
-// Порахує і виведе в консоль кількість категорій в ul#categories,
-//  тобто елементів li.item.
-// Для кожного элемента li.item у спику ul#categories,
-//  знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
-//  і кількість елементів в категорії (усіх <li>, вкладених в нього).
-
-// const elemsOne = document.querySelectorAll('#categories');
-// console.log(elemsOne);
+// // Порахує і виведе в консоль кількість категорій в ul#categories,
+// //  тобто елементів li.item.
+// // Для кожного элемента li.item у спиcку ul#categories,
+// //  знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
+// //  і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
 
-const elemsTwo = document.querySelectorAll('li.item');
-console.log('Number of categories: ' + elemsTwo.length);
 
-// const elements = document.getElementsByTagName('li');
-// console.log (elements);
+const categoriesNumber = document.querySelectorAll('li.item');
+console.log('Number of categories: ' + categoriesNumber.length);
+
+const categoriesName = document.querySelectorAll('li.item>h2');
+categoriesName.forEach(header => {
+console.log('Category: ' + header.textContent);
+const categoriesCount = header.querySelectorAll('ul li');
+console.log('Elements: ' + categoriesCount.length);
+});
 
 
+
+
+// Category: Animals
+// Elements: 4
 
