79d2f0b427a8f617c6328b33030600993817d521
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-21 16:25:18 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,18 @@
-const ingredients = [
-    "Potatoes",
-    "Mushrooms",
-    "Garlic",
-    "Tomatos",
-    "Herbs",
-    "Condiments",
-  ];
+// Порахує і виведе в консоль кількість категорій в ul#categories,
+//  тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories,
+//  знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
+//  і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+// const elemsOne = document.querySelectorAll('#categories');
+// console.log(elemsOne);
+
+
+const elemsTwo = document.querySelectorAll('li.item');
+console.log('Number of categories: ' + elemsTwo.length);
+
+// const elements = document.getElementsByTagName('li');
+// console.log (elements);
+
+
+
