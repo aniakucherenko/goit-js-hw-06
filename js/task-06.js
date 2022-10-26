76df0ad851e7cf6934a0028cfbf38611ae83c969
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-26 19:25:30 +0300

    update

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector("#validation-input");
+const dataLength = textInput.getAttribute("data-length");
+
+textInput.addEventListener("blur", (event) => {
+    if (textInput.value.length === Number(dataLength)) {
+      textInput.classList.add("valid")
+      textInput.classList.remove("invalid")
+    } else {
+      textInput.classList.add("invalid")
+      textInput.classList.remove("valid")
+    }
+    console.log(textInput.value.length);
+ });
+  
