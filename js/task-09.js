c130bacaf42f84a25486e8952ba8b32ea6cd5b78
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-27 00:31:00 +0300

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const changeColor = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+changeColor.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = document.body.style.backgroundColor;
+});
+
+
+ 
