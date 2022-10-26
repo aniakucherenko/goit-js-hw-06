b85471dd42ac38e030cd7e0e6c6eebe915a81aa0
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-27 00:39:38 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,39 +1,47 @@
-// const ingredients = [
-//   'Potatoes',
-//   'Mushrooms',
-//   'Garlic',
-//   'Tomatos',
-//   'Herbs',
-//   'Condiments',
-// ];
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
 
-const elementOne = document.createElement("li");
-elementOne.textContent = 'Potatoes';
-elementOne.classList.add('item');
-
-const elementTwo =document.createElement("li");
-elementTwo.textContent = 'Mushrooms';
-elementTwo.classList.add('item');
-
-const elementThree = document.createElement("li");
-elementThree.textContent = 'Garlic';
-elementThree.classList.add('item');
-
-const elementFour = document.createElement("li");
-elementFour.textContent = 'Tomatos';
-elementFour.classList.add('item');
-
-const elementFive = document.createElement("li");
-elementFive.textContent = 'Herbs';
-elementFive.classList.add('item');
-
-const elementSix = document.createElement("li");
-elementSix.textContent = 'Condiments';
-elementSix.classList.add('item');
-
+const ingredientsArr = ingredients.map(ingredient => {
+    const child =document.createElement("li");
+    child.classList.add('item');
+    child.textContent = ingredient;
+    return child;
+});
 
 ingredients.append(elementOne, elementTwo, elementThree, elementFour, elementFive, elementSix);
 console.log(ingredients);
+
+// const elementOne = document.createElement("li");
+// elementOne.textContent = 'Potatoes';
+// elementOne.classList.add('item');
+
+// const elementTwo =document.createElement("li");
+// elementTwo.textContent = 'Mushrooms';
+// elementTwo.classList.add('item');
+
+// const elementThree = document.createElement("li");
+// elementThree.textContent = 'Garlic';
+// elementThree.classList.add('item');
+
+// const elementFour = document.createElement("li");
+// elementFour.textContent = 'Tomatos';
+// elementFour.classList.add('item');
+
+// const elementFive = document.createElement("li");
+// elementFive.textContent = 'Herbs';
+// elementFive.classList.add('item');
+
+// const elementSix = document.createElement("li");
+// elementSix.textContent = 'Condiments';
+// elementSix.classList.add('item');
+
+
 
 
 // Напиши скрипт, який для кожного елемента
