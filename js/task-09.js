6e2029aa38250fd760f78022c4a02ff21e36aa49
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-28 15:16:58 +0300

    fis task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const changeColor = document.querySelector('.change-color');
 const colorName = document.querySelector('.color');
 
-changeColor.addEventListener("click", function onClick(event) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  colorName.textContent = document.body.style.backgroundColor;
+changeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
 });
-
-
- 
