1991bbe268da93da13e8c6307a3e73f8138b3279
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-28 12:23:44 +0300

    fix task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,10 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
-function IsEmpty() {
-
-        if (document.form.question.value == "") {
-          alert("Anonymous");
-        }
-        return;
-      }
+textInput.addEventListener("input", event => {
+    if (event.currentTarget.value.length === 0) {
+      output.textContent = "Anonymous" 
+    } else {
+      output.textContent = event.currentTarget.value
+    }
 });
